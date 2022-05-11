6ce1afe9d663ed818306aee9ce1585e1348911d7
ToLToL <treqh@outlook.com>
2022-05-11 12:05:39 +0200

    2 simples tests

--- cli/cmd/cli_test.go
+++ cli/cmd/cli_test.go
@@ -1,19 +1,54 @@
 package cmd
 
 import (
+	"bytes"
+	"fmt"
 	"testing"
+	// "os"
 
 	"github.com/spf13/cobra"
 )
 
-func TestExecuteCommand(t *testing.T) {
-	rootCmd := &cobra.Command{
-		Use:   "petra",
-		Short: "Private terraform registry cli",
-		Long:  "CLI to manage terraform modules in our private registry in a Google Cloud Storage bucket.",
-		RunE: func(cmd *cobra.Command, args []string) error {
-			return nil
-		},
+// Utils
+func emptyRun(*cobra.Command, []string) {}
+
+func executeCommand(root *cobra.Command, args ...string) (output string, err error) {
+	_, output, err = executeCommandC(root, args...)
+	return output, err
+}
+
+func executeCommandC(root *cobra.Command, args ...string) (c *cobra.Command, output string, err error) {
+	buf := new(bytes.Buffer)
+	root.SetOut(buf)
+	root.SetErr(buf)
+	root.SetArgs(args)
+
+	c, err = root.ExecuteC()
+
+	return c, buf.String(), err
+}
+
+// Tests
+func TestRootExecuteUnknownCommand(t *testing.T) {
+	rootCmd.AddCommand(UploadCmd())
+
+	output, err := executeCommand(rootCmd, "unknown")
+
+	fmt.Println(err)
+	expected := "Error: unknown command \"unknown\" for \"petra\"\nRun 'petra --help' for usage.\n"
+
+	if output != expected {
+		t.Errorf("\nExpected:\n %q\nGot:\n %q\n", expected, output)
 	}
-	rootCmd.Execute()
+}
+
+func TestUploadSubCmdNoFlag(t *testing.T) {
+	_, err := executeCommand(rootCmd, "upload")
+
+	fmt.Println(err)
+	expected := "required flag(s) \"gcs-bucket\", \"module-directory\" not set"
+
+	if err.Error() != expected {
+		t.Errorf("\nExpected:\n %q\nGot:\n %q\n", expected, err.Error())
+	}
 }
